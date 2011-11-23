a18768321c0c438590e21861fbd481c4a1b9543e
Conn O'Griofa <connogriofa@gmail.com>
2011-11-23 04:14:54 +0000

    Clean up vendor files/use new setup-makefiles.sh method

--- device_u8160.mk
+++ device_u8160.mk
@@ -1,4 +1,3 @@
-#
 # Copyright (C) 2011 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,14 +13,7 @@
 # limitations under the License.
 #
 
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	LOCAL_KERNEL := device/huawei/u8160/prebuilt/kernel
-else
-	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
-endif
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_KERNEL):kernel
+DEVICE_PACKAGE_OVERLAYS += device/huawei/u8160/overlay
 
 PRODUCT_PACKAGES += \
     libOmxCore \
@@ -40,17 +32,6 @@ PRODUCT_PACKAGES += \
     erase_image \
     make_ext4fs \
     e2fsck
-
-# Live Wallpapers
-PRODUCT_PACKAGES += \
-    librs_jni \
-    LiveWallpapers \
-    LiveWallpapersPicker \
-    VisualizationWallpapers
-
-# Live Wallpapers support
-PRODUCT_COPY_FILES += \
-    packages/wallpapers/LivePicker/android.software.live_wallpaper.xml:/system/etc/permissions/android.software.live_wallpaper.xml
 
 # Hardware permissions
 PRODUCT_COPY_FILES += \
@@ -84,72 +65,10 @@ PRODUCT_COPY_FILES += \
     device/huawei/u8160/prebuilt/etc/dhcpcd/dhcpcd.conf:system/etc/dhcpcd/dhcpcd.conf \
     device/huawei/u8160/prebuilt/etc/wifi/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf
 
-# RIL specific
-PRODUCT_COPY_FILES += \
-    vendor/huawei/u8160/proprietary/bin/qmuxd:system/bin/qmuxd \
-    vendor/huawei/u8160/proprietary/bin/rild:system/bin/rild \
-    vendor/huawei/u8160/proprietary/lib/libcm.so:system/lib/libcm.so \
-    vendor/huawei/u8160/proprietary/lib/libdiag.so:system/lib/libdiag.so \
-    vendor/huawei/u8160/proprietary/lib/libdll.so:system/lib/libdll.so \
-    vendor/huawei/u8160/proprietary/lib/libdsm.so:system/lib/libdsm.so \
-    vendor/huawei/u8160/proprietary/lib/libdss.so:system/lib/libdss.so \
-    vendor/huawei/u8160/proprietary/lib/libgsdi_exp.so:system/lib/libgsdi_exp.so \
-    vendor/huawei/u8160/proprietary/lib/libgstk_exp.so:system/lib/libgstk_exp.so \
-    vendor/huawei/u8160/proprietary/lib/libmmgsdilib.so:system/lib/libmmgsdilib.so \
-    vendor/huawei/u8160/proprietary/lib/libnv.so:system/lib/libnv.so \
-    vendor/huawei/u8160/proprietary/lib/liboncrpc.so:system/lib/liboncrpc.so \
-    vendor/huawei/u8160/proprietary/lib/libpbmlib.so:system/lib/libpbmlib.so \
-    vendor/huawei/u8160/proprietary/lib/libqmi.so:system/lib/libqmi.so \
-    vendor/huawei/u8160/proprietary/lib/libqueue.so:system/lib/libqueue.so \
-    vendor/huawei/u8160/proprietary/lib/libril.so:system/lib/libril.so \
-    vendor/huawei/u8160/proprietary/lib/libril.so:obj/lib/libril.so \
-    vendor/huawei/u8160/proprietary/lib/libril-qc-1.so:system/lib/libril-qc-1.so \
-    vendor/huawei/u8160/proprietary/lib/libril-qcril-hook-oem.so:system/lib/libril-qcril-hook-oem.so \
-    vendor/huawei/u8160/proprietary/lib/libwms.so:system/lib/libwms.so \
-    vendor/huawei/u8160/proprietary/lib/libwmsts.so:system/lib/libwmsts.so
-    
-# OMX
-PRODUCT_COPY_FILES += \
-    vendor/huawei/u8160/proprietary/lib/libmm-omxcore.so:system/lib/libmm-omxcore.so \
-    vendor/huawei/u8160/proprietary/lib/libmm-adspsvc.so:system/lib/libmm-adspsvc.so \
-    vendor/huawei/u8160/proprietary/lib/libOmxCore.so:system/lib/libOmxCore.so \
-    vendor/huawei/u8160/proprietary/lib/libOmxH264Dec.so:system/lib/libOmxH264Dec.so \
-    vendor/huawei/u8160/proprietary/lib/libOmxMpeg4Dec.so:system/lib/libOmxMpeg4Dec.so \
-    vendor/huawei/u8160/proprietary/lib/libOmxVidEnc.so:system/lib/libOmxVidEnc.so \
-    vendor/huawei/u8160/proprietary/lib/libOmxWmvDec.so:system/lib/libOmxWmvDec.so
-
-# Hardware drivers
-PRODUCT_COPY_FILES += \
-    vendor/huawei/u8160/proprietary/lib/hw/gralloc.msm7k.so:system/lib/hw/gralloc.msm7k.so 
-
-# Sensors
-PRODUCT_COPY_FILES += \
-    vendor/huawei/u8160/proprietary/bin/akmd2:system/bin/akmd2 \
-    vendor/huawei/u8160/proprietary/bin/akmd8962:system/bin/akmd8962 \
-    vendor/huawei/u8160/proprietary/bin/akmd8975:system/bin/akmd8975 \
-    vendor/huawei/u8160/proprietary/lib/hw/sensors.default.so:system/lib/hw/sensors.default.so 
-
-# Bluetooth
-PRODUCT_COPY_FILES += \
-    vendor/huawei/u8160/proprietary/etc/bluetooth/BCM4329.hcd:system/etc/bluetooth/BCM4329.hcd \
-    vendor/huawei/u8160/proprietary/bin/brcm_patchram_plus:system/bin/brcm_patchram_plus
-
 # Audio
 PRODUCT_COPY_FILES += \
     device/huawei/u8160/prebuilt/etc/AudioFilter.csv:system/etc/AudioFilter.csv \
-    device/huawei/u8160/prebuilt/etc/AudioFilterU8160.csv:system/etc/AudioFilterU8160.csv \
     device/huawei/u8160/prebuilt/etc/AutoVolumeControl.txt:system/etc/AutoVolumeControl.txt \
-    device/huawei/u8160/prebuilt/etc/01_qc.cfg:system/etc/01_qc.cfg \
-    vendor/huawei/u8160/proprietary/lib/libaudioeq.so:system/lib/libaudioeq.so
-
-# Camera
-PRODUCT_COPY_FILES += \
-    vendor/huawei/u8160/proprietary/lib/libcamera.so:obj/lib/libcamera.so \
-    vendor/huawei/u8160/proprietary/lib/libcamera.so:system/lib/libcamera.so \
-    vendor/huawei/u8160/proprietary/lib/libqcamera.so:system/lib/libqcamera.so \
-    vendor/huawei/u8160/proprietary/lib/libmmjpeg.so:system/lib/libmmjpeg.so \
-    vendor/huawei/u8160/proprietary/lib/libmmipl.so:system/lib/libmmipl.so \
-    vendor/huawei/u8160/proprietary/lib/libmmprocess.so:system/lib/libmmprocess.so
 
 # Compcache
 PRODUCT_COPY_FILES += \
@@ -163,30 +82,6 @@ PRODUCT_COPY_FILES += \
     device/huawei/u8160/prebuilt/etc/vold.fstab:system/etc/vold.fstab \
     device/huawei/u8160/prebuilt/etc/init.d/02madteam:system/etc/init.d/02madteam \
     device/huawei/u8160/prebuilt/etc/init.d/05mountsd:system/etc/init.d/05mountsd
-
-$(call inherit-product, build/target/product/full_base.mk)
-
-# Inherit some common cyanogenmod stuff.
-$(call inherit-product, vendor/cyanogen/products/common_full.mk)
-
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_eu_supl.mk)
-
-# Include GSM stuff
-$(call inherit-product, vendor/cyanogen/products/gsm.mk)
-
-# Broadcom FM radio
-$(call inherit-product, vendor/cyanogen/products/bcm_fm_radio.mk)
-
-$(call inherit-product-if-exists, vendor/huawei-u8160/u8160-vendor.mk)
-
-DEVICE_PACKAGE_OVERLAYS += device/huawei/u8160/overlay
-
-# LDPI assets
-PRODUCT_LOCALES += ldpi mdpi
-PRODUCT_PACKAGE_OVERLAYS += vendor/cyanogen/overlay/ldpi
-PRODUCT_COPY_FILES += \
-    vendor/cyanogen/prebuilt/ldpi/media/bootanimation.zip:system/media/bootanimation.zip
 
 # Enable Google-specific location features,
 # like NetworkLocationProvider and LocationCollector
@@ -237,12 +132,27 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.compcache.default=18
 
-# Override product version
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=CyanogenMod-7.1.0-MADTEAM
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/huawei/u8160/prebuilt/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
 
-PRODUCT_NAME := full_u8160
-PRODUCT_BRAND := huawei
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product-if-exists, vendor/huawei/u8160/u8160-vendor.mk)
+
+$(call inherit-product, build/target/product/full_base.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_eu_supl.mk)
+
+# LDPI assets
+PRODUCT_LOCALES += ldpi mdpi
+
+PRODUCT_NAME := huawei_u8160
 PRODUCT_DEVICE := u8160
-PRODUCT_MODEL := u8160
+PRODUCT_MODEL := U8160
+PRODUCT_BRAND := huawei
 PRODUCT_MANUFACTURER := huawei
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -20,4 +20,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cyanogen_u8160-eng
+add_lunch_combo generic_u8160-eng
